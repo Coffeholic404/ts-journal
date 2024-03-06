a68c110c121244a35802a1bbd8c74f6ed863c937
Mohammed Taqi <taqealibade@gmail.com>
2024-03-06 13:47:38 +0300

    Refactoring in TypeScript

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -3,21 +3,36 @@ import EmojiField from "@/components/EmojjiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
 import type Emoji from "@/types/Emoji";
 import { ref, computed} from "vue";
-
-const text = ref("")
+import type Entry from "@/types/Entry";
+// data
+const body = ref("")
 const emoji = ref<Emoji | null>(null)
-const textCount = computed(()=> text.value.length)
+const textCount = computed(()=> body.value.length)
 const maxChar = 280 as number;
+
+//methods
 const handleTextInput = (e: Event) =>{
   e.preventDefault()
   const textarea = e.target as HTMLTextAreaElement;
-  (textarea.value.length <= maxChar) ? text.value = textarea.value : text.value = textarea.value = textarea.value.substring(0, 280);
+  (textarea.value.length <= maxChar) ? body.value = textarea.value : body.value = textarea.value = textarea.value.substring(0, 280);
 }
+
+//Events
+defineEmits<{
+  (e: "@create", entry: Entry): void;
+}>();
+
 </script>
 <template>
-  <form class="entry-form" @submit.prevent>
+  <form class="entry-form" @submit.prevent="$emit('@create', {
+    body,
+    emoji,
+    createAt: new Date(),
+    userId: 1,
+    id: Math.random()
+    })">
     <textarea
-    :value="text" 
+    :value="body" 
     @keyup="handleTextInput" 
     placeholder="New Journal Entry"></textarea>
     <EmojiField v-model="emoji"/>
---
+++ src/types/Entry.ts
@@ -0,0 +1,9 @@
+import type Emoji from "./Emoji";
+
+export default interface Entry{
+    id: number;
+    body: string;
+    emoji: Emoji | null;
+    createAt: Date;
+    userId: number;
+}
